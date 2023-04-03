2bb1c50374a74312bc65cb6d193831bb3e06d85a
irfanzidnioffical <irfanzidniofficial@gmail.com>
2023-04-03 11:24:09 +0700

    first commit

---
+++ bin/dart_oop_learn.dart
@@ -0,0 +1,5 @@
+import 'package:dart_oop_learn/dart_oop_learn.dart' as dart_oop_learn;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_oop_learn.calculate()}!');
+}
---
+++ test/dart_oop_learn_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_oop_learn/dart_oop_learn.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
